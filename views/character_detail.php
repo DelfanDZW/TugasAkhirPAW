a935c3aa44c17fa242b2c904d161a1fed7794e96
DelfanZuffarRajjazNuziar <delfanzuffarrajjaznuziar@gmail.com>
2024-12-02 23:41:22 +0700

    fix2

--- views/character_detail.php
+++ views/character_detail.php
@@ -1,16 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Character Detail</title>
+    <link rel="stylesheet" href="assets/stylechar.css">
+</head>
+<body>
 <div class="film-detail-container">
     <div class="back-button">
         <button onclick="location.href='?c=Character'">&laquo; Kembali</button>
     </div>
-    <div class="film-info">
+    <div>
         <h1><?php echo htmlspecialchars($character->name); ?></h1>
         <p><strong>Nama:</strong> <?php echo htmlspecialchars($character->name); ?></p>
         <p><strong>Tags:</strong> <?php echo htmlspecialchars($character->tags); ?></p>
         <p><strong>Deskripsi:</strong> <?php echo htmlspecialchars($character->description); ?></p>
 
     </div>
-    <div class="film-poster">
+    <div>
         <img src="img/<?php echo htmlspecialchars($character->image); ?>"
             alt="<?php echo htmlspecialchars($character->name); ?>">
     </div>
 </div>
+</body>
+</html>
+
+
+
